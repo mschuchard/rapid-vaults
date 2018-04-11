9f031d0ed634ae665d16ccf7d8296b535f78d1ba
Matt Schuchard <grapheneowns@gmail.com>
2018-04-11 08:39:24 -0400

    fixing decrypt prep context

--- spec/rapid-vaults/decrypt_spec.rb
+++ spec/rapid-vaults/decrypt_spec.rb
@@ -3,16 +3,15 @@ require_relative '../../lib/rapid-vaults/encrypt'
 require_relative '../../lib/rapid-vaults/decrypt'
 
 describe Decrypt do
-  before(:all) do
-    Encrypt.openssl(ui: :cli, file: "foo: bar\n", key: '���b+����R�v�Í%("����=8o/���', nonce: 'Ëá!í^Uë^EÜ<83>oã^M')
-    Encrypt.gpgme(ui: :cli, file: "foo: bar\n")
-  end
-
   after(:all) do
     %w[tag.txt encrypted.txt decrypted.txt].each { |file| File.delete(file) }
   end
 
   context '.openssl' do
+    before(:all) do
+      Encrypt.openssl(ui: :cli, file: "foo: bar\n", key: '���b+����R�v�Í%("����=8o/���', nonce: 'Ëá!í^Uë^EÜ<83>oã^M')
+    end
+
     it 'outputs a decrypted file with the key, nonce, and tag from the cli' do
       Decrypt.openssl(ui: :cli, file: File.read('encrypted.txt'), key: '���b+����R�v�Í%("����=8o/���', nonce: 'Ëá!í^Uë^EÜ<83>oã^M', tag: File.read('tag.txt'))
       expect(File.file?('decrypted.txt')).to be true
@@ -29,13 +28,17 @@ describe Decrypt do
   end
 
   context '.gpgme' do
+    before(:all) do
+      Encrypt.gpgme(ui: :cli, file: "foo: bar\n")
+    end
+
     it 'outputs a decrypted file with the key from the cli' do
       Decrypt.gpgme(ui: :cli, file: File.read('encrypted.txt'), key: '')
       expect(File.file?('decrypted.txt')).to be true
       expect(File.read('decrypted.txt')).to eq("foo: bar\n")
     end
     it 'outputs decrypted content with the key from the api' do
-      decrypt = Decrypt.openssl(ui: :api, file: File.read('encrypted.txt'), key: '')
+      decrypt = Decrypt.gpgme(ui: :api, file: File.read('encrypted.txt'), key: '')
       expect(decrypt).to be_a(String)
       expect(decrypt).to eq("foo: bar\n")
     end
